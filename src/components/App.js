7ea6be2d45a6414c987dc2a8d829496e01cd8e8e
sarapalcam <sara.pal.cam@gmail.com>
2022-01-12 11:00:19 +0100

    add exercise day 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import '../styles/index.scss';
 import '../styles/App.scss';
+import { useState } from 'react';
 
 function App() {
+
+  const [numberOfErrors, setNumberOfErrors] = useState(0);
+
+  const handleClickBtn = () => {
+    setNumberOfErrors(numberOfErrors + 1);
+  }
+
   return (
     <div className="page">
       <header>
@@ -46,7 +54,7 @@ function App() {
             />
           </form>
         </section>
-        <section className="dummy error-5">
+        <section className={`dummy error-${numberOfErrors}`}>
           <span className="error-13 eye"></span>
           <span className="error-12 eye"></span>
           <span className="error-11 line"></span>
@@ -60,6 +68,7 @@ function App() {
           <span className="error-3 line"></span>
           <span className="error-2 line"></span>
           <span className="error-1 line"></span>
+          <button onClick={handleClickBtn}>Incrementar</button>
         </section>
       </main>
     </div>
